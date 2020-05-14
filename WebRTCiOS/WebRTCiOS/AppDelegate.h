e0fc0fbf581f630581194238352d5cf47f9d5d6d
zhangguangpeng <zhangguangpemng503@126.com>
2020-05-14 16:51:40 +0800

    初始化iOS项目

---
+++ WebRTCiOS/WebRTCiOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WebRTCiOS
+//
+//  Created by zhangguangpeng on 2020/5/14.
+//  Copyright © 2020 zhangguangpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ WebRTCiOS/WebRTCiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WebRTCiOS
+//
+//  Created by zhangguangpeng on 2020/5/14.
+//  Copyright © 2020 zhangguangpeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
